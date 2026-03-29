117fb39669572feb52757eaeb1af867398d6933e
Renuka-019 <renukaapatil019@gmail.com>
2026-03-29 12:22:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import Home from "./Home";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
 
   return (
     <BrowserRouter>
       <Routes>
 
-        {/* 🔐 Default → Login */}
-        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-
-        {/* 📝 Signup */}
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
-        {/* 🚀 Protected Home */}
         <Route
           path="/home"
           element={
