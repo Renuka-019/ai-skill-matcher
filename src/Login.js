29da03a5342610cba34a357aedc1336b3e111995
Renuka-019 <renukaapatil019@gmail.com>
2026-04-03 18:54:09 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion } from "framer-motion";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -28,16 +27,28 @@ function Login() {
 
   return (
     <div className="container">
-      <motion.div className="card" initial={{ scale: 0.8 }} animate={{ scale: 1 }}>
-        <h2>🔐 Login</h2>
 
-        <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      {/* LEFT SIDE */}
+      <div className="left">
+        <h1>Welcome Back 👋<br/>AI Skill Matcher</h1>
+      </div>
 
-        <button onClick={handleLogin}>Login</button>
+      {/* RIGHT SIDE */}
+      <div className="right">
+        <div className="card">
+          <h2>Login</h2>
 
-        <p>Don't have account? <a href="/signup">Signup</a></p>
-      </motion.div>
+          <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+
+          <button onClick={handleLogin}>Login</button>
+
+          <p>
+            Don't have account? <a href="/signup">Signup</a>
+          </p>
+        </div>
+      </div>
+
     </div>
   );
 }
