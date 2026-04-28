37712e31312978878edbcabfc48176ee91142d88
Renuka-019 <renukaapatil019@gmail.com>
2026-04-28 18:41:27 +0530

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="bg-gray-800 p-4 text-white flex justify-between">
+      <h1 className="text-xl font-bold">AI Career Platform</h1>
+      <p>User</p>
+    </div>
+  );
+}
